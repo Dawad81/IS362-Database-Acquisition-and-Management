591d27a827b464739cb0eeccce71fba7a3c94877
Dennis Awad <da12263683@IS362-1-1202-13.sps.cuny.edu>
2020-02-02 16:40:12 -0500

    Edited link to Tableau workbook

--- IS362_Assignment1.sql
+++ IS362_Assignment1.sql
@@ -59,4 +59,4 @@ GROUP BY a.name, f.tailnum , p.manufacturer, f.year, f.month, f.day
 ORDER BY a.name, f.year, f.month, f.day;
 
 -- Link to Tableau workbook representing this data 
--- https://public.tableau.com/views/IS361Assignment1/Dashboard1?:display_count=y&publish=yes&:origin=viz_share_link
+-- https://public.tableau.com/shared/H9J9XBCHM?:display_count=y&:origin=viz_share_link
